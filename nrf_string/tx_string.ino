804252b38eed61889adfc6a321835118055225c5
Shanmugavalli P <shanmugavalli871@gmail.com>
2026-07-04 00:02:22 +0530

    Create tx_string.ino
    Implemented and tested transmission of a string  using NRF24L01 and Arduino UNO.

---
+++ nrf_string/tx_string.ino
@@ -0,0 +1,46 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define CE_PIN 9
+#define CSN_PIN 10
+
+RF24 radio(CE_PIN, CSN_PIN);
+
+const byte address[6] = "00001";
+
+char data []= "Hello";
+
+
+void setup()
+{
+  Serial.begin(9600);
+
+  if (!radio.begin())
+  {
+    Serial.println("NRF NOT DETECTED");
+    while (1);
+  }
+
+  radio.setChannel(76);
+  radio.setDataRate(RF24_1MBPS);
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setAutoAck(false);
+
+  radio.openWritingPipe(address);
+  radio.stopListening();
+
+  Serial.println("TX READY");
+}
+
+void loop()
+{
+  bool ok = radio.write(&data, sizeof(data));
+
+  if (ok)
+    Serial.println("TX SUCCESS");
+  else
+    Serial.println("TX FAILED");
+
+  delay(1000);
+}
